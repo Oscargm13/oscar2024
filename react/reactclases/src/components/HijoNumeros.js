05224ac7ee692f7b956f76af1291f21d4563f8db
Oscargm13 <oskitargm5@gmail.com>
2024-10-15 10:03:53 +0200

    cambios15/10

--- react/reactclases/src/components/HijoNumeros.js
+++ react/reactclases/src/components/HijoNumeros.js
@@ -1,13 +1,17 @@
 import { Component } from "react";
 
 class HijoNumeros extends Component {
-    
+    sumarNumeroHijo = () => {
+        var num = parseInt(this.props.numero);
+        console.log(num)
+        this.props.sumarNumero(num);
+    }
 
     render(){
         return(
             <div>
                 <h2 style={{color: "green"}}>{parseInt(this.props.numero)}</h2>
-                <button>Cambiar numero</button>
+                <button onClick={this.sumarNumeroHijo}>Sumar {this.props.numero}</button>
             </div>
         )
     }
--- react/reactclases/src/components/PadreNumeros.js
+++ react/reactclases/src/components/PadreNumeros.js
@@ -4,7 +4,14 @@ import HijoNumeros from "./HijoNumeros";
 
 class PadreNumeros extends Component {
     state = {
-        numeros: [1, 2]
+        numeros: [1, 2],
+        suma: 0
+    }
+
+    sumarNumero = (valor) => {
+        this.setState({
+            suma: this.state.suma + valor
+        })
     }
     
     generarNumeros = () => {
@@ -22,10 +29,11 @@ class PadreNumeros extends Component {
         return (
             <div>
                 <h1 style={{color: "red"}}>PADRE NUMEROS</h1>
+                <h2>La suma de los numeros es: {this.state.suma}</h2>
                 <button onClick={this.generarNumeros}>Generar número</button>
                 {                    
                     this.state.numeros.map((numero, index) => {
-                        return (<HijoNumeros key={index} numero={numero}/>)
+                        return (<HijoNumeros key={index} numero={numero} sumarNumero={this.sumarNumero}/>)
                     })
                 }
             </div>
